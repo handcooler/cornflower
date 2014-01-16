6fd6bae5d6a00b6e725a875183ec2ad984ec1c78
sanemat <o.gata.ken@gmail.com>
2014-01-16 09:21:19 +0900

    Add comment

--- bin/gem_list.rb
+++ bin/gem_list.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+# http://stackoverflow.com/questions/21094095/how-to-get-all-gems-names-via-web
 require 'rubygems/spec_fetcher'
 fetcher = Gem::SpecFetcher.fetcher
 tuples = fetcher.detect(:released) { true }
