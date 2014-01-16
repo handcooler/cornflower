7321ac40593c5e54f93531356203f358af745c11
sanemat <o.gata.ken@gmail.com>
2014-01-16 12:48:18 +0900

    Use multi_json

--- bin/gem_list.rb
+++ bin/gem_list.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 require 'bundler/setup'
 require 'pry'
+require 'multi_json'
 
 # http://stackoverflow.com/questions/21094095/how-to-get-all-gems-names-via-web
 require 'rubygems/spec_fetcher'
 fetcher = Gem::SpecFetcher.fetcher
 tuples = fetcher.detect(:latest) { true }
-tuples.map{ |tuple| tuple.first }
-
-binding.pry
+holder = {}
+tuples.map do |tuple|
+  gem = tuple.first
+  holder << { name: gem.name, platform: gem.platform, version: gem.version.to_s }
+end
+puts MultiJson.dump(holder)
